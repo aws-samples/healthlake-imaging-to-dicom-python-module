332a0d73dd67e7cbeb13aa294e6b912e19b34c83
Ubuntu <ubuntu@ip-172-31-77-27.ec2.internal>
2023-01-23 16:23:53 +0000

    Initial commit.

--- example/main.py
+++ example/main.py
@@ -13,7 +13,7 @@ def main():
     
     datastoreId = "7d997634041974df7c23aa016833ccee" #Replace this value with your datastoreId
     imageSetId = "91e80f552ad9fbfa525103f3c0d368ae" #Replace this value with your imageSetId
-    AHLIEndpoint = "https://iad.gamma.medical-imaging.ai.aws.dev"  # Can be set to None if the default AHLI endpoint is used.
+    AHLIEndpoint = None  # Can be set to None if the default AHLI endpoint is used.
 
     # Default values for Frame Fetcher and DICOMizer processes count.
     # Frame Fetcher : Number of Parallelize processes to fetchand decompress the HTJ2K frames from AHLI. If Set to None the default value will be 4 x number of cores.
